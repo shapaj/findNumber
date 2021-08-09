1718a18206a546945680b6b315f190962d0d6d0c
Igor Shovkoplias <41335097+shapaj@users.noreply.github.com>
2021-08-09 17:47:10 +0300

    Add files via upload

---
+++ ownAPP/controllers/GameViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var timer: UILabel!
+    
+    @IBOutlet var GameButtons: [UIButton]!
+    
+    
+    
+    let user = CurentUser.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+        
+        
+    }
+    
+    @IBAction func onChoseNumber(_ sender: Any) {
+      
+        
+    }
+}
